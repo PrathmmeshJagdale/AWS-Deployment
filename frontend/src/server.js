1b8baab4ff0a79089456b06c00fca3b5c4ac5d86
PrathmmeshJagdale <jagdaleprathmmesh@gmail.com>
2026-01-30 14:47:52 +0000

    K8s with Minikube

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const app = express();
 
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000/api/message";
+const BACKEND_URL = process.env.BACKEND_URL || "http://backend:5000/api/message";
 
 app.get("/health", (req, res) => {
     res.json({ status: "UP" });
